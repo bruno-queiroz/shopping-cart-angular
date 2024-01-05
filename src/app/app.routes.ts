77e2c93a1de0c2ef7ec93f3f69acd71ecdbe4319
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-01-05 15:53:38 -0300

    Add /store route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { StoreComponent } from './store/store.component';
 
 export const routes: Routes = [
     {
         path: "",
         component: HomeComponent,
-        title: "Title"
+        title: "Home | Shopping Cart"
+    },
+    {
+        path: "store",
+        component: StoreComponent,
+        title: "Store | Shopping Cart" 
     }
 ];
