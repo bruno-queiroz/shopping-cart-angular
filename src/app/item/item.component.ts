fb1a8c0209d52a0cb15adb1b5e96e37d880dd732
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-01-10 07:29:43 -0300

    Create isItemAdded state and amount

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,13 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { StoreItem } from '../store/store.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './item.component.html',
   styleUrl: './item.component.css'
 })
 export class ItemComponent {
   @Input() item!: StoreItem;
+  isItemAdded = true;
+  amount = 1;
+
+  addItemToCart(){
+    this.isItemAdded = true
+    console.log('adding item')
+  }
 }
