6035837368b88c3c44faeec9dc94b9a23be205c3
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-01-12 07:58:12 -0300

    Create isCartOpen state

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { CartComponent } from '../cart/cart.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, CartComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isCartOpen = false;
 
+  openCart(){
+    this.isCartOpen = true;
+  }
+
+  closeCart(){
+    this.isCartOpen = false;
+  }
 }
