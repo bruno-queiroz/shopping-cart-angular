4f06d9c371b6a405feeb3336777695149815c7a9
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-01-15 07:33:05 -0300

    Create cart item component

---
+++ src/app/cart-item/cart-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-cart-item',
+  standalone: true,
+  imports: [ MatIconModule ],
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.css'
+})
+export class CartItemComponent {
+
+}
