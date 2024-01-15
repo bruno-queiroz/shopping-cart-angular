0ae94da451bf2fa2430c7ea964995d9f886fc557
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-01-15 07:33:16 -0300

    Add cart item component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { CartItemComponent } from '../cart-item/cart-item.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
