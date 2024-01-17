19167cd8c5baa0ae647c3894c08e3da50d8de4e7
Bruno Queiroz <brunoqueiroz612@gmail.com>
2024-01-17 07:52:36 -0300

    Inject cart service to cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,18 +1,26 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { CartItemComponent } from '../cart-item/cart-item.component';
+import { CartItem, CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, MatIconModule, CartItemComponent],
+  imports: [ CommonModule, MatIconModule, CartItemComponent ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  cartService: CartService = inject(CartService);
+  cartItems: CartItem[] = [];
+
   @Input() isCartOpen!: boolean;
   @Output() closeCartEvent = new EventEmitter();
+
+  constructor(){
+    this.cartItems = this.cartService.items;
+  }
 
   closeCart() {
     this.closeCartEvent.emit();
